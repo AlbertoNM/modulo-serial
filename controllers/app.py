6f26b8890b2a5efed9b9bfc9c41a3042e1d84454
Alberto NM <alberto-city@hotmail.com>
2024-09-10 15:05:41 -0600

    Detect serial ports

--- controllers/app.py
+++ controllers/app.py
@@ -5,6 +5,9 @@ from PyQt5.QtWidgets import QMainWindow
 # Components imports
 from components.mainwindow import Ui_MainWindow
 
+# Imports
+import serial.tools.list_ports
+
 
 class MainApp(QMainWindow):
     """
@@ -17,5 +20,15 @@ class MainApp(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
-    
+        self.fill_combo_box_with_ports()
+
+    def fill_combo_box_with_ports(self):
+        # Obtener lista de puertos seriales disponibles
+        puertos = serial.tools.list_ports.comports()
+
+        # Agregar cada puerto disponible al ComboBox
+        for puerto in puertos:
+            # Formato: "Puerto (Descripción)"
+            item_text = f"{puerto.device} ({puerto.description})"
+            self.ui.PuertoCB.addItem(item_text)
     
